e6bc25f67ea3d9c2f89445c719a39a2550f119f1
federicoDiG <federicodigiovanni95@gmail.com>
2024-11-08 17:08:41 +0100

    fix

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -141,8 +141,6 @@ const Info = () => {
 
                 <div className="video_container">
                     <iframe
-                        width="560"
-                        height="315"
                         src="https://www.youtube.com/embed/FzZiTpxBowQ?si=P0nLd_t7fEWokUZJ"
                         title="YouTube video player"
                         frameborder="0"
